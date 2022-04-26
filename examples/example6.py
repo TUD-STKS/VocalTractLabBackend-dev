d0255b80b54dc3b3d5867533c993b45380889382
Konstantin (Tino) Sering <konstantin.sering@uni-tuebingen.de>
2022-04-26 14:03:36 +0200

    resovles conflict

--- examples/example6.py
+++ examples/example6.py
@@ -96,7 +96,9 @@ print("NOTE: for this part you need `blender` and the python packages "
 # change the `convert_directory.py` to your liking and then execute it on the
 # Terminal with blender
 if sys.platform.startswith('win32'):
-    os.system(r'"D:\Program Files\Blender Foundation\Blender 3.0\blender.exe" --background --python Meshes/convert_directory.py')
+    command = r'"D:\Program Files\Blender Foundation\Blender 3.0\blender.exe" --background --python Meshes/convert_directory.py'
+    print(f"check that the following command points to the right Blender installation (if not change the line in example6.py):\n\n{command}")
+    os.system(command)
 else:
     os.system('blender --background --python Meshes/convert_directory.py 1> /dev/null')
 
